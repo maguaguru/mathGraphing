a9d0e22a3bec70384bf991da529dd6aa7d92a87f
Ruslan Makarov <rmakarov@wiley.com>
2018-09-28 18:42:06 +0300

    NOTRACK: Added react sceleton

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import SVGContainer from './modules/SVGContainer';
+import  * as testData  from './static/testData'
+
+class App extends Component {
+
+  render() {
+    console.log('testData: ', testData)
+    return (
+      <div className="App">
+        <h2>Test SVG components</h2>
+        <SVGContainer width="500px" height="500px" content={testData}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ControlSpot.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ControlSpot = () =>
+  <circle
+      cx="50"
+      cy="50"
+      r="40"
+      stroke="blue"
+      strokeWidth="4"
+      fill="lightblue"
+    />
